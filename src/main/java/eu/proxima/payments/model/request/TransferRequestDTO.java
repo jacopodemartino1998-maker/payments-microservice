b6c5cee62d3950f622b2fc46d1fbc15048b54c0e
flavio a <flavio@example.com>
2025-12-02 16:44:14 +0100

    classi in entrata create

---
+++ src/main/java/eu/proxima/payments/model/request/TransferRequestDTO.java
@@ -0,0 +1,22 @@
+package eu.proxima.payments.model.request;
+
+import java.sql.Date;
+
+import eu.proxima.payments.enums.CurrencyType;
+import eu.proxima.payments.enums.EntryType;
+import eu.proxima.payments.enums.TransactionType;
+import eu.proxima.payments.model.generiscsinterface.TransactionGenericsDetailDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferRequestDTO {
+    private String sourceIban;
+    private String beneficiaryIban; 
+    private double amount;
+    private String message;
+    private boolean isInstant; 
+}
