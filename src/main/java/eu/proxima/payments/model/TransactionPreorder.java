57505622bad9437505aad15a998e95bf3cf2c3ee
flavio a <flavio@example.com>
2025-12-02 16:35:43 +0100

    crea classi dto per richiesta

--- src/main/java/eu/proxima/payments/model/TransactionPreorder.java
+++ src/main/java/eu/proxima/payments/model/TransactionPreorder.java
@@ -4,15 +4,25 @@ import java.sql.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import eu.proxima.payments.enums.CurrencyType;
+import eu.proxima.payments.enums.EntryType;
+import eu.proxima.payments.enums.TransactionType;
+import eu.proxima.payments.model.generiscsinterface.TransactionGenericsDetailDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "pre_orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionPreorder {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
