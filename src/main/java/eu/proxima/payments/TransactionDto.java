c5d6b79a91ed1a11bbaf95d58f960bd01851474c
jacopodemartino1998-maker <jacopodemartino1998@gmail.com>
2025-12-02 14:32:43 +0100

    test-push

--- src/main/java/eu/proxima/payments/TransactionDto.java
+++ src/main/java/eu/proxima/payments/TransactionDto.java
@@ -1,48 +1,15 @@
 package eu.proxima.payments;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionDto {
 	private long id;
 	private String state;
 	private String message;
-
-	public TransactionDto() {
-		super();
-	}
-
-	public TransactionDto(long id, String state, String message) {
-		super();
-		this.id = id;
-		this.state = state;
-		this.message = message;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	@Override
-	public String toString() {
-		return "TransactionDto [id=" + id + ", state=" + state + ", message=" + message + "]";
-	}
 
 }
