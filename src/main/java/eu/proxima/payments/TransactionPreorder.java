0a5888efcf42090b0b94f7d7e64fdc4e6193cee6
flavio a <flavio@example.com>
2025-12-02 15:55:05 +0100

    creata entity preorders

---
+++ src/main/java/eu/proxima/payments/TransactionPreorder.java
@@ -0,0 +1,30 @@
+package eu.proxima.payments;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pre_orders")
+public class TransactionPreorder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@Column(name = "preOrder_id")
+	private long id;
+	@Column(name="beneficiary_iban" , nullable = false)
+	private String beneficiary_iban;
+	@Column(name = "amount",nullable = false)
+	private double amount;
+	@Column(name = "message")
+	private String message;
+	@Column(name = "execute_at",nullable = false, updatable = false)
+	@CreationTimestamp
+	private Date executedAt;
+}
